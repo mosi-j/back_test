7f076f6daadd49cebedaca60168d42eb2a4813eb
mostafa <mosi_4224@yahoo.com>
2019-09-30 08:25:12 +0330

    clean bt_setting.py

--- bt_setting.py
+++ bt_setting.py
@@ -1,9 +1,5 @@
-#from constant_database_data import *
-# from constant_database_data import *
 from server_setting import analyze_db_info
 
 
 bt_database_info = analyze_db_info
-# bt_database_info = laptop_analyze_server_role_db_info
-#bt_database_info = constant_database_data.laptop_analyze_server_role_db_info
 
